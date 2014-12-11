d5a8cfeee16ac65d439b7b624aadd64fc793f444
Rumman Ahmed <ahmed.rumman@gmail.com>
2014-12-11 17:25:47 +1100

    production format ready -- app.js no strict mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,10 +226,7 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
             '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/public/assets/fonts/*',
-            '<%= yeoman.dist %>/public/assets/css/*',
-            '<%= yeoman.dist %>/public/assets/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/public/assets/js/*'
+            '<%= yeoman.dist %>/public/assets/fonts/*'
           ]
         }
       }
